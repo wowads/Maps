05ab62ac6414ea1968fdaf6b68e17cd9fa6f16c9
Jan Galek <admin@gcore.cz>
2020-10-26 13:44:05 +0100

    Added workflow

---
+++ DB/Types.lua
@@ -0,0 +1,19 @@
+---
+--- Created by warfacez.
+--- DateTime: 25.10.20 10:18
+---
+MAPS_TYPE_COSMIC = 0
+MAPS_TYPE_WORLD = 1
+MAPS_TYPE_CONTINENT = 2
+MAPS_TYPE_ZONE = 3
+MAPS_TYPE_DUNGEON = 4
+MAPS_TYPE_MICRO = 5
+MAPS_TYPE_ORPHAN = 6
+
+addType(MAPS_TYPE_COSMIC, "Cosmic", "")
+addType(MAPS_TYPE_WORLD, "World", "")
+addType(MAPS_TYPE_CONTINENT, "Continent", "")
+addType(MAPS_TYPE_ZONE, "Zone", "")
+addType(MAPS_TYPE_DUNGEON, "Dungeon", "")
+addType(MAPS_TYPE_MICRO, "Micro", "")
+addType(MAPS_TYPE_ORPHAN, "Orphan", "")
